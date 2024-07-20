dcded2574723e697007752f7e45169eb67b60431
tashvit <tashvit@users.noreply.github.com>
2024-07-20 10:47:05 +0100

    A3 model - pix2pix transfer learning

--- common_functions.py
+++ common_functions.py
@@ -1,18 +1,89 @@
 from PIL import Image
+import cv2
+import numpy as np
+from resizeimage import resizeimage
+import os
+
+
+# https://stackoverflow.com/a/74205492
+
+def create_sketch(cv_img, levels=3, magenta_bg=True):
+    image = cv_img
+
+    # Create a basic sketch
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    invert_image = cv2.bitwise_not(gray_image)
+    blur_image = cv2.GaussianBlur(invert_image, (5, 5), 0)
+    invert_blur = cv2.bitwise_not(blur_image)
+    sketch = cv2.divide(gray_image, invert_blur, scale=256.0)
+
+    # perform edge detection on the sketch
+    b1 = cv2.GaussianBlur(sketch, (5, 5), 0)
+    b2 = b1
+    for _ in range(0, levels - 1):
+        b2 = cv2.GaussianBlur(b2, (5, 5), 0)
+    edge = cv2.Canny(b2, 50, 100)
+    edge_inv = cv2.bitwise_not(edge)
+    edge_inv = cv2.cvtColor(edge_inv, cv2.COLOR_GRAY2BGR)
+    if magenta_bg:
+        # Define the white color range
+        lower_white = np.array([254, 254, 254], dtype=np.uint8)
+        upper_white = np.array([255, 255, 255], dtype=np.uint8)
+
+        # Create a mask to detect white regions
+        mask = cv2.inRange(edge_inv, lower_white, upper_white)
+
+        # Change white regions to magenta
+        edge_inv[mask == 255] = [255, 0, 255]
+    return edge_inv
+
+
+def write_image(image, filename):
+    if not os.path.exists(os.path.dirname(filename)):
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+    if isinstance(image, Image.Image):
+        image.save(filename)
+    else:
+        cv2.imwrite(filename, image)
+
+
+def pil_to_opencv(pil_img):
+    cv2_img = np.array(pil_img)
+    cv2_img = cv2.cvtColor(cv2_img, cv2.COLOR_RGB2BGR)
+    return cv2_img
+
+
+def opencv_to_pil(cv2_img):
+    cv2_img = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
+    pil_img = Image.fromarray(cv2_img)
+    return pil_img
+
+
+def load_with_magenta_background(input_img_path):
+    # Convert input image to RGBA
+    img = Image.open(input_img_path).convert("RGBA")
+    # New image with a magenta background
+    magenta_bg = Image.new("RGBA", img.size, (255, 0, 255, 255))
+    # Get input image on top of magenta image
+    img_with_magenta_bg = Image.alpha_composite(magenta_bg, img)
+    # Convert back to RGB to remove alpha channel
+    final_img = img_with_magenta_bg.convert("RGB").convert("RGBA")
+    return final_img
+
+
+def load_64x64_with_magenta_bg(image_path):
+    image = load_with_magenta_background(image_path)
+    res = resizeimage.resize_contain(image, [64, 64], bg_color=(255, 0, 255, 255))
+    return res
+
+
+def to_64x64_magenta(image_path, output_path):
+    res = load_64x64_with_magenta_bg(image_path)
+    # Save image to output directory
+    res.save(output_path)
 
 
 def turn_magenta(input_img_path, output_img_path):
-    # Convert input image to RGBA
-    img = Image.open(input_img_path).convert("RGBA")
-
-    # New image with a magenta background
-    magenta_bg = Image.new("RGBA", img.size, (255, 0, 255, 255))
-
-    # Get input image on top of magenta image
-    img_with_magenta_bg = Image.alpha_composite(magenta_bg, img)
-
-    # Convert back to RGB to remove alpha channel
-    final_img = img_with_magenta_bg.convert("RGB")
-
+    final_img = load_with_magenta_background(input_img_path)
     # Save image to output directory
     final_img.save(output_img_path)
--- experiments.md
+++ experiments.md
@@ -20,7 +20,6 @@ python pix2pix/train.py --dataroot ./pix2pix_data/AB --name pixel --model pix2pi
 ```bash
 python pix2pix/train.py --dataroot ./pix2pix_data/AB --name pixel_A2 --model pix2pix --direction BtoA --netG unet_64 --netD pixel --load_size 64 --crop_size 64 --display_winsize 64
 ```
-
 ### Model B1
 
 * Generator - 'unet_64' 
@@ -49,3 +48,28 @@ python pix2pix/train.py --dataroot ./model_c_data/combined --name pixel_C1 --mod
 ```bash
 python pix2pix/train.py --dataroot ./model_d_data/combined --name pixel_D1 --model pix2pix --direction AtoB --netG unet_64 --netD pixel --load_size 64 --crop_size 64 --display_winsize 64 --no_flip
 ```
+
+
+### Model A3
+
+* Generator - 'unet_64' 
+* Discriminator is 1x1 PatchGAN/pixel 
+* Do not resize images to 256x256
+* Two-step training
+  * Mixed data - Anime Faces, Pokemon, Some game assets (Kenny.nl)
+  * Pixel art character front facing data
+* Epochs are customized
+* Continue training from mixed data.
+
+#### Step 1 - Train on subset of AFD + Pokemon
+
+```bash
+python pix2pix/train.py --dataroot ./input_a3/tl_combined --name pixel_A3 --model pix2pix --direction AtoB --netG unet_64 --netD pixel --load_size 64 --crop_size 64 --display_winsize 64 --n_epochs 40 --n_epochs_decay 40
+```
+
+#### Step 2 - Train on pixel art characters
+
+```bash
+python pix2pix/train.py --dataroot ./input_a3/pix2pix_combined --name pixel_A3 --model pix2pix --direction AtoB --netG unet_64 --netD pixel --load_size 64 --crop_size 64 --display_winsize 64 --continue_train --epoch_count 41 --n_epochs 60 --n_epochs_decay 60
+```
+
